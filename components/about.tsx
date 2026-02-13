b677185d5c35a596fe492138d63f2e6bab7db863
ftcaleb <caleb19scott@gmail.com>
2026-02-13 14:31:24 +0200

    updated image type

--- components/about.tsx
+++ components/about.tsx
@@ -21,7 +21,7 @@ export function About() {
           <MotionDiv variants={slideInLeft} className="relative">
             <div className="relative rounded-2xl overflow-hidden aspect-[4/3] shadow-2xl">
               <Image
-                src="/asset/img1.jpg"
+                src="/asset/img1.webp"
                 alt="Students collaborating in a modern study space"
                 fill
                 className="object-cover"
