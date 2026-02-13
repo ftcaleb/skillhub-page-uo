6c5e29fd4d824e012d7f616227075ba665d68d82
ftcaleb <caleb19scott@gmail.com>
2026-02-13 12:47:33 +0200

    updatee

--- lib/gallery-data.ts
+++ lib/gallery-data.ts
@@ -10,55 +10,55 @@ export const galleryCategories = ["All", "Community", "Certified", "Workshops",]
 export const galleryItems: GalleryItem[] = [
     {
         id: "1",
-        src: "/asset/bgimg2.jpeg",
+        src: "/asset/bgimg2.webp",
         category: "Events",
         span: "large"
     },
     {
         id: "2",
-        src: "/asset/img1.jpg",
+        src: "/asset/img1.webp",
         category: "Certified",
         span: "medium"
     },
     {
         id: "3",
-        src: "/asset/img9.jpeg",
+        src: "/asset/img9.webp",
         category: "Workshops",
         span: "small"
     },
     {
         id: "4",
-        src: "/asset/img11.jpg",
+        src: "/asset/img11.webp",
         category: "Certified",
         span: "small"
     },
     {
         id: "5",
-        src: "/asset/img15.jpeg",
+        src: "/asset/img15.webp",
         category: "Workshops",
         span: "medium"
     },
     {
         id: "6",
-        src: "/asset/img5.jpeg",
+        src: "/asset/img5.webp",
         category: "Community",
         span: "large"
     },
     {
         id: "7",
-        src: "/asset/img16.jpeg",
+        src: "/asset/img16.webp",
         category: "Community",
         span: "small"
     },
     {
         id: "9",
-        src: "/asset/img17.jpeg",
+        src: "/asset/img17.webp",
         category: "Certified",
         span: "small"
     },
     {
         id: "8",
-        src: "/asset/img7.jpeg",
+        src: "/asset/img7.webp",
         category: "Facilities",
         span: "medium"
     }
